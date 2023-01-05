0444d7447d8f515c455b4f27e523e7ae9c0b2f9c
Felipe Uribe <wfpaisa@gmail.com>
2023-01-05 09:11:21 -0500

    🚀 RELEASE: redirect 4

---
+++ docs/assets/use-quasar.d5a21a12.js
@@ -0,0 +1,1 @@
+import{i as a,am as r}from"./index.5398fc1b.js";function u(){return a(r)}export{u};
--- docs/assets/use-quasar.fa6931b3.js
+++
@@ -1,1 +0,0 @@
-import{i as a,am as r}from"./index.5929446c.js";function u(){return a(r)}export{u};
