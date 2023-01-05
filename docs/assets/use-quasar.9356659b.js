be97da1852fa82642ce45951bb2c2bd645e936f4
Felipe Uribe <wfpaisa@gmail.com>
2023-01-05 13:13:34 -0500

    web2

---
+++ docs/assets/use-quasar.9356659b.js
@@ -0,0 +1,1 @@
+import{i as a,an as r}from"./index.8914391d.js";function u(){return a(r)}export{u};
--- docs/assets/use-quasar.f4de0fb7.js
+++
@@ -1,1 +0,0 @@
-import{i as a,an as r}from"./index.c0dab7b4.js";function u(){return a(r)}export{u};
